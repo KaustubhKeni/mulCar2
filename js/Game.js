29eafadfe312eab64011a32186752e75d39909e1
KaustubhKeni <66947556+KaustubhKeni@users.noreply.github.com>
2020-09-14 18:59:13 +0530

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -26,12 +26,13 @@ class Game {
       form = new Form()
       form.display();
     }
-  }
-car1=createSprite(100,200);
+    car1=createSprite(100,200);
 car2=createSprite(300,200);
 car3=createSprite(500,200);
 car4=createSprite(700,200);
 cars=[car1,car2,car3,car4];
+  }
+
   play(){
     form.hide();
     textSize(30);
